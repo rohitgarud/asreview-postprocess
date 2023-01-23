49b1422a2265e5232e47c3020e2d2dcfe6b503f2
rohitgarud <rohit.garuda1992@gmail.com>
2023-01-23 12:57:37 +0530

    Add option to select all or included records
    Also formatted using Black formatting

--- asreviewcontrib/postprocess/entrypoint.py
+++ asreviewcontrib/postprocess/entrypoint.py
@@ -14,23 +14,40 @@ class KeywordsEntryPoint(BaseEntryPoint):
     @property
     def version(self):
         from asreviewcontrib.postprocess.__init__ import __version__
+
         return __version__
 
     def execute(self, argv):
-        parser = argparse.ArgumentParser(prog='asreview keywords')
+        parser = argparse.ArgumentParser(prog="asreview keywords")
 
-        parser.add_argument('asreview_files',
-                            metavar='asreview_files',
-                            type=str,
-                            nargs='+',
-                            help='A (list of) ASReview files')
-        
+        parser.add_argument(
+            "asreview_files",
+            metavar="asreview_files",
+            type=str,
+            nargs="+",
+            help="A (list of) ASReview files",
+        )
+
         parser.add_argument(
             "-m",
             "--method",
             dest="method",
             default="tf-idf",
-            help='Method for extracting keywords (default = tf-idf)'
+            help="Method for extracting keywords (default = tf-idf)",
+        )
+
+        parser.add_argument(
+            "--all",
+            dest="use_all_records",
+            action="store_true",
+            help="Select all records from the dataset for keyword extraction",
+        )
+
+        parser.add_argument(
+            "--included",
+            dest="use_all_records",
+            action="store_false",
+            help="Select only included records from the dataset for keyword extraction",
         )
 
         parser.add_argument(
@@ -44,29 +61,35 @@ class KeywordsEntryPoint(BaseEntryPoint):
             "-o",
             "--output",
             dest="outputfile_name",
-            help='Output file name or path. Currently only .csv files are supported.')
+            help="Output file name or path. Currently only .csv files are supported.",
+        )
 
         args = parser.parse_args(argv)
-        
+
         if len(args.asreview_files) > 1:
-            raise ValueError("Extracting keywords from multiple project files"
-                             " via the CLI is not supported yet.")
-            
+            raise ValueError(
+                "Extracting keywords from multiple project files"
+                " via the CLI is not supported yet."
+            )
+
         asreview_filename = args.asreview_files[0]
-            
+
         if args.outputfile_name:
             outputfile_name = args.outputfile_name
-            if not outputfile_name.endswith('.csv'):
-                if '.' in outputfile_name:
-                    raise ValueError("File extensions other than .csv are not supported yet")
+            if not outputfile_name.endswith(".csv"):
+                if "." in outputfile_name:
+                    raise ValueError(
+                        "File extensions other than .csv are not supported yet"
+                    )
                 else:
-                    outputfile_name += '.csv'
+                    outputfile_name += ".csv"
         else:
             outputfile_name = os.path.basename(asreview_filename)
             outputfile_name = f"{os.path.splitext(outputfile_name)[0]}-with-keywords-{datetime.now().strftime('%Y%m%dT%H%M')}.csv"
-                
+
         extract_keywords(
             asreview_filename=asreview_filename,
             outputfile_name=outputfile_name,
-            method=args.method
+            method=args.method,
+            use_all_records=args.use_all_records,
         )
--- asreviewcontrib/postprocess/keywords.py
+++ asreviewcontrib/postprocess/keywords.py
@@ -12,43 +12,58 @@ from sklearn.feature_extraction.text import TfidfVectorizer
 from fuzzywuzzy import process
 from asreviewcontrib.postprocess.utils import pre_process
 
-def extract_keywords(asreview_filename, outputfile_name, method):
+
+def extract_keywords(asreview_filename, outputfile_name, method, use_all_records=False):
     project_path = Path("tmp_data")
+    try:
+        shutil.rmtree(project_path)
+    except:
+        pass
+
     project_path.mkdir(parents=True, exist_ok=True)
     project = ASReviewProject.load(asreview_filename, project_path)
-    
-    dataset_fp = Path(project_path, project.config['id'], "data", project.config['dataset_path'])
+
+    dataset_fp = Path(
+        project_path, project.config["id"], "data", project.config["dataset_path"]
+    )
     dataset = ASReviewData.from_file(dataset_fp)
-        
+
     with open_state(asreview_filename) as state:
         state_df = state.get_dataset()
-        state_df['labeling_order'] = state_df.index
-        state_df.rename(columns={'label':'current_label'}, inplace=True)
-             
-    dataset_w_labels = dataset.df.join(state_df.set_index('record_id')[['current_label']], on='record_id')
-    
-   # combine title and abstract into text
+        state_df["labeling_order"] = state_df.index
+        state_df.rename(columns={"label": "current_label"}, inplace=True)
+
+    dataset_w_labels = dataset.df.join(
+        state_df.set_index("record_id")[["current_label"]], on="record_id"
+    )
+
+    # combine title and abstract into text
     dataset_w_labels["text"] = dataset.title + " " + dataset.abstract
-    dataset_included = dataset_w_labels[dataset_w_labels['current_label'] == 1].copy()
-     # Applying preprocessing to the dataset
+    if use_all_records:
+        dataset_included = dataset_w_labels.copy()
+    else:
+        dataset_included = dataset_w_labels[
+            dataset_w_labels["current_label"] == 1
+        ].copy()
+
+    # Applying preprocessing to the dataset
     dataset_included["text"] = dataset_included["text"].apply(pre_process)
-    dataset_included.drop(['current_label'], axis=1, inplace=True)
-    
+
     corpus = dataset_included["text"].values
-            
+
+    dataset_included.drop(["current_label", "text"], axis=1, inplace=True)
+
     if method == "tf-idf":
         # Initialising TF-IDF vectorizer
         vectorizer = TfidfVectorizer(
-        stop_words="english",
-        max_df=0.8,
-        ngram_range=(1,3)
+            stop_words="english", max_df=0.8, ngram_range=(1, 3), max_features=10000
         )
         # Fit and transform the text
         tfidf = vectorizer.fit_transform(corpus)
 
         # Get the feature names
         feature_names = vectorizer.get_feature_names_out()
-        
+
         # Get top 10 keywords for the document and reduce duplication
         extracted_keywords = []
         for doc_tfidf in tfidf.toarray():
@@ -57,12 +72,15 @@ def extract_keywords(asreview_filename, outputfile_name, method):
             deduplicated_doc_keywords = list(process.dedupe(doc_keywords, threshold=70))
             final_keywords = ", ".join(deduplicated_doc_keywords)
             extracted_keywords.append(final_keywords)
-        
-        dataset_included = dataset_included.assign(extracted_keywords = extracted_keywords)
-            
+
+        dataset_included = dataset_included.assign(
+            extracted_keywords=extracted_keywords
+        )
+
     else:
-        raise ValueError("This keyword extraction method is not implemented. Please select 'tf-idf'.")
-    
+        raise ValueError(
+            "This keyword extraction method is not implemented. Please select 'tf-idf'."
+        )
+
     dataset_included.to_csv(outputfile_name)
     shutil.rmtree(project_path)
-        
