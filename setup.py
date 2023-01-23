4d7852be07200ebce70845fce083e306b09fd835
rohitgarud <rohit.garuda1992@gmail.com>
2023-01-23 15:54:46 +0530

    Add rake requirements to setup.py

--- setup.py
+++ setup.py
@@ -13,44 +13,47 @@ import versioneer
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the README file
-with open(path.join(here, 'README.md'), encoding='utf-8') as f:
+with open(path.join(here, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-    name='asreview-postprocess',
+    name="asreview-postprocess",
     version=versioneer.get_version(),
     cmdclass=versioneer.get_cmdclass(),
-    description='ASReview postprocesing extension',
+    description="ASReview postprocesing extension",
     long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/rohitgarud/asreview-postprocess',
-    author='Rohit Garud',
-    author_email='rohit.garuda1992@gmail.com',
+    long_description_content_type="text/markdown",
+    url="https://github.com/rohitgarud/asreview-postprocess",
+    author="Rohit Garud",
+    author_email="rohit.garuda1992@gmail.com",
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        'License :: OSI Approved :: Apache Software License',
-        'Programming Language :: Python :: 3.7',
-        'Programming Language :: Python :: 3.8',
-        'Programming Language :: Python :: 3.9',
-        'Programming Language :: Python :: 3.10',
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: Apache Software License",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
     ],
-    keywords='asreview postprocess',
-    packages=find_namespace_packages(include=['asreviewcontrib.*']),
+    keywords="asreview postprocess",
+    packages=find_namespace_packages(include=["asreviewcontrib.*"]),
     install_requires=[
         "asreview>=1,<2",
         "numpy",
         "scikit-learn",
         "pandas",
-        "fuzzywuzzy"
+        "fuzzywuzzy",
     ],
-    extras_require={},
+    extras_require={
+        "rake": ["nltk", "rake_nltk"],
+        "all": ["nltk", "rake_nltk"],
+    },
     entry_points={
         "asreview.entry_points": [
             "keywords = asreviewcontrib.postprocess.entrypoint:KeywordsEntryPoint",
         ]
     },
     project_urls={
-        'Bug Reports': "https://github.com/rohitgarud/asreview-postprocess/issues",
-        'Source': "https://github.com/rohitgarud/asreview-postprocess",
+        "Bug Reports": "https://github.com/rohitgarud/asreview-postprocess/issues",
+        "Source": "https://github.com/rohitgarud/asreview-postprocess",
     },
 )
