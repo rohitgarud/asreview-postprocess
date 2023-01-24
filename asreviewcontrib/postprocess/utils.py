2f9a11fff8379f69277659d100607e549d117d41
rohitgarud <rohit.garuda1992@gmail.com>
2023-01-24 22:15:47 +0530

    Add comprehensive stopwords and robust preprocessing

--- asreviewcontrib/postprocess/utils.py
+++ asreviewcontrib/postprocess/utils.py
@@ -1,7 +1,18 @@
 import re
 
-def pre_process(text):
-    # remove punctuations and digits 
-    text = re.sub("[^a-zA-Z]", " ", text)
-    return text
+with open("stopwords-en.txt", encoding="utf-8") as sw:
+    STOPWORDS_EN = sw.readlines()
+    STOPWORDS_EN = [word.replace("\n", "") for word in STOPWORDS_EN]
 
+
+def pre_process(text):
+    text = text.lower()
+    text = text.split()
+    # removing stopwords before removing punctuations because
+    # stopwords include many words with apostrophe
+    cleaned_text = [word for word in text if word not in STOPWORDS_EN]
+    text = " ".join(cleaned_text)
+    # remove punctuations and digits
+    text = re.sub("[^a-zA-Z]", " ", text)
+    text = re.sub(" +", " ", text)
+    return text
